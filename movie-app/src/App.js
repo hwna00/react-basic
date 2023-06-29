156940bff8ce32d104ca2789eef5e2650cda4d0c
hwna00 <layton3534@gmail.com>
2023-06-29 20:28:46 +0900

    Movie App part Two

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,40 +1,7 @@
-import { useEffect, useState } from "react";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading((prev) => !prev);
-  };
-  useEffect(() => getMovies, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {" "}
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <h2>{movie.title}</h2>
-              <img src={movie.medium_cover_image} alt="img" />
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ movie-app/src/components/Movie.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Movie({ title, medium_cover_image, summary, genres }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <img src={medium_cover_image} alt="img" />
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  medium_cover_image: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
