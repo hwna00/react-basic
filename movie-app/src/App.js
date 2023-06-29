c0665329e21f419e036391dd08a9fd41655ff950
hwna00 <layton3534@gmail.com>
2023-06-29 17:45:54 +0900

    Todo List Part One

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>My Todos ({todos.length})</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add Todo</button>
+      </form>
+      <ul></ul>
     </div>
   );
 }
