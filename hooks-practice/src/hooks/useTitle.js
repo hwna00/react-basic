349717cbaf662a6b185fcc092f4872654f77dfce
hwna00 <layton3534@gmail.com>
2023-07-01 13:19:53 +0900

    Make useTitle

--- hooks-practice/src/hooks/useTitle.js
+++ hooks-practice/src/hooks/useTitle.js
@@ -10,16 +10,4 @@ const useTitle = (initialTitle) => {
   return setTitle;
 };
 
-function App() {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => {
-    titleUpdater("Home");
-  }, 1000);
-  return (
-    <div className="App">
-      <div>Hi</div>
-    </div>
-  );
-}
-
-export default App;
+export default useTitle;
