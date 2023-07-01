14974990c4bf83a71904b2c289d908a3eb6d589c
hwna00 <layton3534@gmail.com>
2023-07-01 19:27:30 +0900

    Make useNetwork

--- hooks-practice/src/hooks/useNetwork.js
+++ hooks-practice/src/hooks/useNetwork.js
@@ -20,16 +20,4 @@ const useNetwork = (onChange) => {
   return status;
 };
 
-function App() {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We just went online" : "We are offline");
-  };
-  const onLine = useNetwork(handleNetworkChange);
-  return (
-    <div className="App">
-      <h1>{onLine ? "Online" : "Offline"}</h1>
-    </div>
-  );
-}
-
-export default App;
+export default useNetwork;
