d3737e41664aab988a60b8a76d348714b4d2cda5
hwna00 <layton3534@gmail.com>
2023-07-01 19:15:05 +0900

    Make useFadeIn

--- hooks-practice/src/hooks/useFadeIn.js
+++ hooks-practice/src/hooks/useFadeIn.js
@@ -15,15 +15,4 @@ const useFadeIn = (duration = 1, delay = 0) => {
   return { ref: element, style: { opacity: 0 } };
 };
 
-function App() {
-  const fadeInH1 = useFadeIn(3, 2);
-  const fadeInP = useFadeIn(5, 3);
-  return (
-    <div className="App">
-      <h1 {...fadeInH1}>Hi</h1>
-      <p {...fadeInP}>lorem ipsum lakdljflsj</p>
-    </div>
-  );
-}
-
-export default App;
+export default useFadeIn;
