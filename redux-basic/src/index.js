8b29d1211a5505e6377c4e951f33372fe3035c60
hwna00 <layton3534@gmail.com>
2023-07-03 17:50:38 +0900

    Make pure-counter

--- redux-basic/src/index.js
+++ redux-basic/src/index.js
@@ -1,16 +1,38 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+const initialState = { value: 0 };
+number.innerText = initialState.value;
 
-const handleAdd = () => {
-  number.innerText = ++count;
-};
-const handleMinus = () => {
-  number.innerText = --count;
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      number.innerText = ++state.value;
+    },
+    decrement: (state) => {
+      number.innerText = --state.value;
+    },
+  },
+});
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const { increment, decrement } = counterSlice.actions;
+const counterReducer = counterSlice.reducer;
+
+const counterStore = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+plus.addEventListener("click", () => {
+  counterStore.dispatch(increment());
+});
+
+minus.addEventListener("click", () => {
+  counterStore.dispatch(decrement());
+});
