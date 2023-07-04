5ee803635b3b8340e44fc0a5b518dce22954120a
hwna00 <layton3534@gmail.com>
2023-07-04 18:31:55 +0900

    #3.3 mapDispatchToProps

--- redux-basic/src/routes/Home.js
+++ redux-basic/src/routes/Home.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home(props) {
-  console.log(props);
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
-    console.log(text);
+    setText("");
+    addToDo(text);
   }
   return (
     <>
@@ -18,12 +19,19 @@ function Home(props) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
-function mapStateToProps(state, ownPros) {
+function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+    deleteToDo: (id) => dispatch(actionCreators.deleteToDo(id)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
