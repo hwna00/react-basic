d18d37f2c5d7d13d476654a198b1567f918fcde1
hwna00 <layton3534@gmail.com>
2023-07-04 20:51:08 +0900

    #3.5 Detail Screen

--- redux-basic/src/routes/Detail.js
+++ redux-basic/src/routes/Detail.js
@@ -1,5 +1,20 @@
-function Detail() {
-  return "Detail";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+  const { id } = useParams();
+  const toDo = toDos?.find((toDo) => toDo.id === parseInt(id));
+  console.log(toDo);
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  );
 }
 
-export default Detail;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
