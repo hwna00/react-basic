2edc785068a169e678310eeab8552de7c892b37a
hwna00 <layton3534@gmail.com>
2023-07-04 17:27:09 +0900

    Initialize React Redux

--- redux-basic/src/index.js
+++ redux-basic/src/index.js
@@ -1,56 +1,10 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const initialState = { todos: [] };
-
-const listSlice = createSlice({
-  name: "list",
-  initialState,
-  reducers: {
-    addItem: (state, action) => {
-      state.todos = [...state.todos, { text: action.payload, id: Date.now() }];
-    },
-    deleteItem: (state, action) => {
-      console.log("delete item");
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-    },
-  },
-});
-
-const { addItem, deleteItem } = listSlice.actions;
-const listReducer = listSlice.reducer;
-
-const listStore = configureStore({
-  reducer: {
-    list: listReducer,
-  },
-});
-
-const paintTodos = () => {
-  const todos = listStore.getState().list.todos;
-  ul.innerHTML = "";
-  todos.forEach((todo) => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", (event) => {
-      // const id = event.target.parentNode.id;
-      listStore.dispatch(deleteItem(todo.id));
-    });
-    li.id = todo.id;
-    li.innerText = todo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-listStore.subscribe(paintTodos);
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const text = input.value;
-  input.value = "";
-  listStore.dispatch(addItem(text));
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
